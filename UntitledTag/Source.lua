fcf1854bd54676d22f58b3023130a620500a13df
Asterisk <100814109+Zer0ids@users.noreply.github.com>
2023-02-02 20:18:18 -0600

    Update source.lua
    Removed print message; replaced with fake kick message

--- UntitledTag/Source.lua
+++ UntitledTag/Source.lua
@@ -1,6 +1,30 @@
 -- This was the first version of the utg script
 local pathFile = "https://raw.githubusercontent.com/Zer0ids/Qwerty/main/UntitledTag/NewScript.lua"
 
-print("Outdated file. Reloading new version...")
-wait(1)
-loadstring(game:HttpGet(pathFile))()
+local module = getrenv().require(game:GetService("CoreGui").RobloxGui.Modules.ErrorPrompt)
+local prompt = module.new("Default")
+prompt._hideErrorCode = true
+local index = Instance.new("ScreenGui",gethui and gethui() or game.CoreGui)
+index.ResetOnSpawn = false
+index.LayoutOrder = 999
+prompt:setParent(index)
+prmompt:setErrorTitle("Ranxware Hub")
+prompt:_open("This source is outdated. Do you want to load the new one, instead?")
+prompt:updateButtons({
+    {
+      Text="Yes",
+      Callback=function()
+        prompt:_close()
+        index:Destroy()
+        loadstring(game:HttpGet(pathFile))()
+      end,
+      Primary=true
+    },
+    {
+      Text="No thanks.",
+      Callback=function()
+        prompt:_close()
+        index:Destroy()
+      end
+    }
+})
